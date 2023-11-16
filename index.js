66e0cc2423e3875591ad6ed9d441884e5863741f
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-11-16 17:34:49 -0500

    node practice

---
+++ index.js
@@ -0,0 +1,18 @@
+/*var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(8080);
+
+console.log('This example is different!');
+console.log('The result is displayed in the Command Line Interface');*/
+
+var http = require('http');
+var dt = require('./myFirstModule');
+console.log(dt)
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("The date and time are currently: " + dt.myDateTime());
+  res.end();
+}).listen(8080);
